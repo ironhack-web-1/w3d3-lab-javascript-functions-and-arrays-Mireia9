34f5a4afb0a26b51287a2e9f2d6530373542a4aa
Mireia Bertran <mbertran9@gmail.com>
2022-07-23 15:56:14 +0200

    iteracion 1

--- src/functions-and-arrays.js
+++ src/functions-and-arrays.js
@@ -1,7 +1,17 @@
 // Iteration #1: Find the maximum
-function maxOfTwoNumbers() {}
+function maxOfTwoNumbers(number1, number2) {
+  let maxNumber;
+  if (number1 < number2){
+    maxNumber = number2;
+  }else{
+    maxNumber = number1;
+  }
 
+  console.log(maxNumber);
 
+}
+
+maxOfTwoNumbers(5, 10);
 
 // Iteration #2: Find longest word
 const words = ['mystery', 'brother', 'aviator', 'crocodile', 'pearl', 'orchard', 'crackpot'];
